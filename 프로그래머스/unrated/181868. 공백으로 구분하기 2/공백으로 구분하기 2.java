5de17c9353aa85259a47c360bddec6993efdfbad
621 <ljh7a21@gmail.com>
2023-12-30 17:32:01 +0900

    [level unrated] Title: 공백으로 구분하기 2, Time: 0.07 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
+++ 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -1,13 +1,6 @@
 import java.util.Arrays;
 class Solution {
     public String[] solution(String my_string) {
-        String[] stringArray = my_string.split(" ");
-        String[] answer = Arrays.stream(stringArray)
-            .map(String::trim)
-            .toArray(String[]::new);
-        answer = Arrays.stream(answer)
-            .filter(s->!s.isEmpty())
-            .toArray(String[]::new);
-        return answer;
+        return my_string.trim().split("[ ]+");
     }
 }
