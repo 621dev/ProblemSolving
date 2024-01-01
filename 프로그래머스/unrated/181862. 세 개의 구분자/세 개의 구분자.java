8b1705e8b8a8220859dd052820992214008fd25c
621 <ljh7a21@gmail.com>
2024-01-01 22:48:47 +0900

    [level unrated] Title: 세 개의 구분자, Time: 69.25 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.java
+++ 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.java
@@ -5,26 +5,7 @@ import java.util.stream.Collectors;
 
 class Solution {
     public String[] solution(String myStr) {
-        String[] myStrList = myStr.split("a");
-        List<String> strList = new ArrayList<String>(List.of(myStrList));
-        strList = joinString(strList, "b");
-        strList = joinString(strList, "c");
-
-        if(strList.isEmpty()){
-            return new String[]{"EMPTY"};
-        }
-        return strList.toArray(new String[0]);
-    }
-
-    public List<String> joinString(List<String> strArr, String splStr){
-        List<String> strArray = new ArrayList<String>();
-        for(String str:strArr){
-            String[] strArrTemp = str.split(splStr);
-            strArray.addAll(Arrays.asList(strArrTemp));
-        }
-        strArray = strArray.stream()
-                .filter(s -> !s.isEmpty())
-                .collect(Collectors.toList());
-        return strArray;
+        String[] arr = Arrays.stream(myStr.split("[abc]+")).filter(str -> !str.isEmpty()).toArray(String[]::new);
+        return arr.length == 0 ? new String[] { "EMPTY" } : arr;
     }
 }
