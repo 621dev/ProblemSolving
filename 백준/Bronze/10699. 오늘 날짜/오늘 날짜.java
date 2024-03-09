a61dbe918883377ae2980e38e75f60f9f70e5c42
621 <ljh7a21@gmail.com>
2024-03-09 10:26:53 +0900

    [Bronze V] Title: 오늘 날짜, Time: 196 ms, Memory: 17228 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,12 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+        System.out.println(now);
+    }
+}
