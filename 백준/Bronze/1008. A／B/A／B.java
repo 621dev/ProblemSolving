bf5d96fb79dda2b275c1b5df7ca7991a70652672
621 <ljh7a21@gmail.com>
2024-03-11 11:01:00 +0900

    [Bronze V] Title: A/B, Time: 220 ms, Memory: 17972 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.java
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -2,11 +2,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        int[] numbers = new int[8];
+
         Scanner sc = new Scanner(System.in);
-        double a, b, c;
-        a = sc.nextDouble();
-        b = sc.nextDouble();
+        // INPUT
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        // -INPUT
+        sc.close();
+
         System.out.println(a/b);
     }
 }
-
